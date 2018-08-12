6a1b2bea229bd1e2462480002c0074cf873317ee
moimikey <michael.hertzberg@gizmodomedia.com>
2018-08-12 18:54:11 -0400

    use react-error-boundary

--- client/modules/App.js
+++ client/modules/App.js
@@ -1,22 +1,20 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import ErrorBoundary from './ErrorBoundary'
+import ErrorBoundary from 'react-error-boundary'
 import Home from './Home'
 
 function App () {
   return (
     <React.Fragment>
+      <Helmet defaultTitle='Welcome'>
+        <meta charSet='utf-8' />
+      </Helmet>
       <ErrorBoundary>
-        <Helmet defaultTitle='Welcome'>
-          <meta charSet='utf-8' />
-        </Helmet>
-        <ErrorBoundary>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Redirect to='/' />
-          </Switch>
-        </ErrorBoundary>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Redirect to='/' />
+        </Switch>
       </ErrorBoundary>
     </React.Fragment>
   )
